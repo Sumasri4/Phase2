99617b1d69348129fec79956c354c66ec29a0591
Sumasri4 <160507961+Sumasri4@users.noreply.github.com>
2024-06-01 15:08:51 +0530

    Add files via upload

---
+++ day 2/arith operations using stack.py
@@ -0,0 +1,24 @@
+s="5678+-*"   #postfix expression
+st=[]
+for i in s:
+    if i.isdigit():
+        st.append(int(i))
+    elif i=="-":
+        a=st.pop()
+        b=st.pop()
+        st.append(a-b)
+    elif i=="+":
+        a=st.pop()
+        b=st.pop()
+        st.append(a+b)
+    elif i=="*":
+        a=st.pop()
+        b=st.pop()
+        st.append(a*b)
+    elif i=="/":
+        a=st.pop()
+        b=st.pop()
+        st.append(a/b)
+print(st[0])
+
+    
---
+++ day 2/def code.py
@@ -0,0 +1,10 @@
+class student:
+    def __init__(self,roll,sec):
+        self.roll=roll
+        self.sec=sec
+    def study(self):
+        print("i study and my roll is",self.roll)
+rahul=student(12,"a")
+rahul.study()
+priyanka=student(13,"b")
+priyanka.study()
